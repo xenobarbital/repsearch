485f14521350d52ea90f2ebd061810ad14667d2d
David <greencross.am@gmail.com>
2018-10-08 16:41:03 +0400

    Refine fetch API

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FetchApi from './helpers/FetchApi';
 
 class App extends Component {
   // testing
   componentWillMount() {
-    this.getData('RNRF/react-native-router-flux');
-  }
-
-  async getData(repName) {
-    try {
-      const response = await fetch(`https://api.github.com/repos/${repName}`);
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        const repData = {
-          full_name: jsonResponse.full_name,
-          url: jsonResponse.url,
-          forks: jsonResponse.forks,
-          forks_count: jsonResponse.forks_count
-        }
-        console.log(repData);
-        // redux connection
-      } else {
-        throw new Error('Rep request failed!');
-      }
-    } catch (e) {
-      console.log('Rep EGGOG!', e);
-    }
-
-    try {
-      const response = await fetch(`https://api.github.com/repos/${repName}/forks`);
-      if (response.ok) {
-        const jsonResponse = await response.json();
-        const forkData = jsonResponse.map(elem => ({
-          full_name: elem.full_name,
-          html_url: elem.html_url,
-          stargazers_count: elem.stargazers_count,
-          owner_login: elem.owner.login,
-          owner_url: elem.owner.url,
-          owner_avatar: elem.owner.avatar_url
-        }));
-        console.log(forkData);
-        // redux connection
-      } else {
-        throw new Error('Forks request failed!');
-      }
-    } catch (e) {
-      console.log('Forks EGGOG!', e);
-    }
+    FetchApi.getData('RNRF/react-native-router-flux');
   }
 
   render() {
---
+++ src/helpers/FetchApi.js
@@ -0,0 +1,48 @@
+export default class FetchApi{
+  static async getRepData(repName) {
+    try {
+      const response = await fetch(`https://api.github.com/repos/${repName}`);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const repData = {
+          full_name: jsonResponse.full_name,
+          url: jsonResponse.url,
+          forks: jsonResponse.forks,
+          forks_count: jsonResponse.forks_count
+        }
+        return repData;
+      }
+      throw new Error('Rep request failed!');
+    } catch (e) {
+      console.log('Rep EGGOG!', e);
+    }
+  }
+
+  static async getForksData(repName) {
+    try {
+      const response = await fetch(`https://api.github.com/repos/${repName}/forks`);
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        const forksData = jsonResponse.map(elem => ({
+          full_name: elem.full_name,
+          html_url: elem.html_url,
+          stargazers_count: elem.stargazers_count,
+          owner_login: elem.owner.login,
+          owner_url: elem.owner.url,
+          owner_avatar: elem.owner.avatar_url
+        }));
+        return forksData;
+      }
+      throw new Error('Forks request failed!');
+    } catch (e) {
+      console.log('Forks EGGOG!', e);
+    }
+  }
+
+  static async getData(repName) {
+    const repData = await FetchApi.getRepData(repName);
+    const forksData = await FetchApi.getForksData(repName);
+    console.log(repData);
+    console.log(forksData);
+  }
+}
