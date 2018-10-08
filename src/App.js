54ab3ed2b1f697379c4e19972aff7459ca90ff49
David <greencross.am@gmail.com>
2018-10-08 17:08:29 +0400

    Setup the routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import {Route, Link} from 'react-router-dom';
+import Home from './routes/Home';
+import SearchResults from './routes/SearchResults';
 import logo from './logo.svg';
 import './App.css';
 import FetchApi from './helpers/FetchApi';
@@ -9,26 +12,44 @@ class App extends Component {
     FetchApi.getData('RNRF/react-native-router-flux');
   }
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+  render = () => (
+    <div>
+      <div>
+        <ul>
+          <li>
+            <Link to={'/'}>Home</Link>
+          </li>
+          <li>
+            <Link to={'/searchResults'}>Search results</Link>
+          </li>
+        </ul>
       </div>
-    );
-  }
+
+      <Route exact path={'/'} component={Home} />
+      <Route path={'/searchResults'} component={SearchResults} />
+    </div>
+  )
+
+  // render() {
+  //   return (
+  //     <div className="App">
+  //       <header className="App-header">
+  //         <img src={logo} className="App-logo" alt="logo" />
+  //         <p>
+  //           Edit <code>src/App.js</code> and save to reload.
+  //         </p>
+  //         <a
+  //           className="App-link"
+  //           href="https://reactjs.org"
+  //           target="_blank"
+  //           rel="noopener noreferrer"
+  //         >
+  //           Learn React
+  //         </a>
+  //       </header>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
---
+++ src/routes/SearchResults.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class SearchResults extends Component {
+  render = () => (
+    <div>
+      <p>SEARCH RESULTS</p>
+    </div>
+  )
+}
